0cc6307c7ded069c93ad9ec73426c674a519a822
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-08-05 05:28:13 +0700

    urq (uqr) With (In) R Software

---
+++ urq/urq.R
@@ -0,0 +1,18 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Unconditional Quantile Regression Use urq (uqr) With (In) R Software
+install.packages("remotes")
+remotes::install_github("cran/uqr")
+library("uqr")
+uqr = read.csv("https://raw.githubusercontent.com/timbulwidodostp/urq/main/urq/urq.csv",sep = ";")
+# Estimation Unconditional Quantile Regression Use uqr With (In) R Software
+formula = urq ~ urq_
+urq = urq(formula, data = urq)
+urq$coefficients
+urq$tau
+# Unconditional Quantile Regression Use uqr With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
